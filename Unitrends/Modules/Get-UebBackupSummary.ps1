38f46c8f5d379591a23008db1af2088893ee4dcf
bobfrankly <kenisis@hotmail.com>
2016-10-06 13:12:55 -0700

    Adding functionality for physical machine backups
    Added a "Type" to include the app_type so sorting can be done on physical or virtual machines. Modified the logic to use $thisname for name assignment, so it can pull from the correct area for VMs vs Physical

--- Unitrends/Modules/Get-UebBackupSummary.ps1
+++ Unitrends/Modules/Get-UebBackupSummary.ps1
@@ -1,4 +1,4 @@
-function Get-UebBackupSummary {
+function Get-UebBackupSummary1 {
 	[CmdletBinding()]
 	param(
         [string]$rpo = 24
@@ -17,7 +17,13 @@ function Get-UebBackupSummary {
 		$lastbackup = $i.backups | Sort-Object -Property start_date -Descending | Select-Object -First 1
 		$backup_date = [datetime] $lastbackup.start_date
 		$rpa = New-TimeSpan -Start $backup_date -End $date	
-        
+        $thisName = $i.database_name
+        if ($i.app_type -eq "Physical Server"){
+            $thisname =  $i.client_name
+        }
+        # Need a if statement to alter the "VM" name if this is a physical machine
+
+
         if($rpa -le $rpo) {
             $rpo_compliance = "OK"
         } else {
@@ -26,11 +32,12 @@ function Get-UebBackupSummary {
             	
 
 		$Object = New-Object PSObject -Property @{ 
-			VM = $i.database_name
+			VM = $thisname
 			Backups = $i.backups.count
 			RPO_Compliance = $rpo_compliance
 			RPA = "$($rpa.days)d $($rpa.hours)h"
 			RPAspan = $rpa
+			Type = $i.app_type #Physical or Virtual
 		}
 
 		$vmlist += $Object
