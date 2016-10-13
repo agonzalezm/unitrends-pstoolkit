8952daf8e239218e798b73752abdee184db00e2e
Alberto Gonzalez <agonzalez@unitrends.com>
2016-10-13 12:11:27 +0200

    rename uebrestorejob to uebrestorefile added start-uebrestoresql

--- Unitrends/Modules/Start-UebRestoreSql.ps1
+++ Unitrends/Modules/Start-UebRestoreSql.ps1
@@ -1,8 +1,8 @@
 <#
 .Synopsis
-   Submits a Restore Job
+   Submits a SQL Restore Job
 .DESCRIPTION
-
+    Start-UebRestoreSql -backupID 11148 -clientID 35 -instance_name WIR02\SQLRDR -database ReliableDR
 .PARAMETER SID
     System ID of the Unitrends Appliance.
 .PARAMETER BackupID
@@ -11,23 +11,19 @@
     ID of the Client Machine
 .PARAMETER Directory
     Location on the server to re-direct this restore to. If blank, restore will go to where it was backed up from.
-.PARAMETER Includes
-    List of file paths to include into the restore
-.PARAMETER Excludes
-    List of file paths to exclude from the restore
 .EXAMPLE
-    #Need one/More
+    Start-UebRestoreSql -backupID 11148 -clientID 35 -instance_name WIR02\SQLRDR -database ReliableDR
 #>
-function Start-UebRestoreJob{
+function Start-UebRestoreSql{
 param(
     [Parameter(Mandatory=$false,ValueFromPipelineByPropertyName=$true)]
     [int]$sid = 1,
     [Parameter(Mandatory=$true,ValueFromPipelineByPropertyName=$true)]
     [int]$backupID,
     [int]$clientID,
-    [string]$directory,
-    [string[]]$includes,
-    [string[]]$excludes
+    [string]$instance_name,
+    [string]$database,
+    [string]$directory
 )
 # Verb Options?   Start- Submit-
 
@@ -38,28 +34,16 @@ param(
 # Build the Restore Object, make $target, $theseIncludes, $theseExcludes, then make $body out of all and more
 # Build Target
 $target = @{
-    flat = $true
-    non_destructive = $true
-    newer = $true
-    today = $false
-    unix = $true
+    instance = $instance_name
+    database = $database
     directory = $directory
 }
-
-#Build Include(s) and Exclude(s)
-[array]$theseIncludes = $includes
-[array]$theseExcludes = $excludes
 
 #Build Body
 $body = @{
     backup_id = $backupID
     client_id = $clientID
     target = $target
-    before_cmd = ""
-    after_cmd = ""
-    synthesis = $true
-    includes = $theseIncludes
-    excludes = $theseExcludes
 }
 #endregion RestoreObject
 
