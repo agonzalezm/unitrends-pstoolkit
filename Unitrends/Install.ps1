0257b4da3879eca0ed36e7965d00f8cb3146c869
Alberto Gonzalez <agonzalezm@gmail.com>
2018-03-01 00:19:13 +0100

    use http to download

--- Unitrends/Install.ps1
+++ Unitrends/Install.ps1
@@ -2,7 +2,7 @@
 
 Write-Host "Downloading latest version of Unitrends PSToolkit ..." -ForegroundColor Cyan
 $webclient = New-Object System.Net.WebClient
-$url = "https://github.com/Unitrends/unitrends-pstoolkit/archive/master.zip"
+$url = "http://github.com/Unitrends/unitrends-pstoolkit/archive/master.zip"
 $file = "$($env:TEMP)\Unitrends-pstoolkit.zip"
 $webclient.DownloadFile($url,$file)
 
