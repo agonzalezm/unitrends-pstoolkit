2b9dbce9b5f943c0332ad50f6b91780ce84d5a49
Alberto Gonzalez <agonzalez@unitrends.com>
2018-03-09 01:02:17 +0100

    name based on instance_id + _ + asset_name

--- Unitrends/Sample_Scripts/run_replica_id.ps1
+++ Unitrends/Sample_Scripts/run_replica_id.ps1
@@ -9,7 +9,7 @@ $user = "root"
 $pass = "password"
 #hyperv client to restore to replicas
 $client_id=43
-$replica_name_prefix="ueb02"
+$replica_name_prefix="customer1"
 $restore_path="C:/vmtest/replica/"
 $switch_name="test-vswitch"
 #--- end of user settings ------
@@ -59,7 +59,7 @@ Connect-UebServer -Server $ueb -User $user -Password $pass
 $catalog = Get-UebCatalog -InstanceId $instance
 $backup_date = [datetime]::Parse($catalog.last_backup_date).ToString("yyyyMMdd_HHmmss")
 $backup_id = $catalog.last_backup_id
-$vm_name = $replica_name_prefix + "_" + $catalog.asset + "_" + $backup_date
+$vm_name = $replica_name_prefix + "_" + $catalog.asset_id + "_" + $catalog.asset + "_" + $backup_date
 $directory = $restore_path + $vm_name
 
 $vm = Get-VM|where-object {$_.name -eq $vm_name}
@@ -77,7 +77,7 @@ if (Test-Path $directory -PathType Container) {
 
 Write-Progress -Id $instance -Activity $instance -Status "Restoring backup_id $backup_id to $directory"  -PercentComplete 0 -completed
 
-$restore = Start-UebRestoreFile -backupID $catalog.last_backup_id -clientID $client_id -directory $directory -flat $true
+$restore = Start-UebRestoreFile -backupID $catalog.last_backup_id -clientID $client_id -directory $directory -flat $true -synthesis $false
 $restore_id = $restore.id
 
 $restore_job = $null
@@ -98,8 +98,10 @@ Write-Progress  -Id $instance -Activity $instance -Status "Restoring backup_id $
 # restore complete,change vm id, remove saved state, change disk path and register vm or other import incompatibilities
 Write-Progress  -Id $instance -Activity $instance -Status "Import VM as $vm_name"  -PercentComplete 100 -completed
 
+
 $new_guid = [guid]::NewGuid().ToString().ToUpper()
 
+# check if restored VM is vmcx (hv2016) or xml (hv2012)
 $vmcx = Get-Item -Path "$directory\*.preCheckpointCopy"
 $vm_config = ""
 if($vmcx)  {
@@ -147,8 +149,9 @@ Remove-Item -Path "$directory\*.vmcx"
 Remove-Item -Path "$directory\*.vmrs"
 
 
+
 # remove previous restores
-$vm_prefix = $replica_name_prefix + "_" + $catalog.asset + "_*"
+$vm_prefix = $replica_name_prefix + "_" + $catalog.asset_id + "_" + $catalog.asset + "_*"
 $vms = get-vm -name $vm_prefix|Sort-Object -Descending|Select-Object -Skip 1
 
 foreach ($vm in $vms)
